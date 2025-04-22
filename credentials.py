0c068e4c765e380fb4a6041b07dd7522b457d995
erfan moshiri <erfanmoshiri99@gmail.com>
2025-04-22 19:09:24 +1000

    removed credentials file

--- credentials.py
+++
@@ -1,6 +0,0 @@
-ALATION_AT_YOUR_COMPANY_URL = 'https://domain.ap.alationcloud.com/'
-refresh_token = 'Svs-Noq0kkSrP7-RGSTG8NJTk-GfJTlPkwiuWCL4JDTvzCckWU0iM0eaXMob7b1cKmscP0bPlFxgshbXH5XrbA'
-user_id = 1321  # your user id
-
-snowflake_username="erfan.moshiri"
-snowflake_password="re61407.ap-southeast-2"
